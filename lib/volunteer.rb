366c11b44a0817e4692cfda5f05b870eb38cf8f8
Dave Lindqvist <Lindqvist.Dave@gmail.com>
2021-07-30 09:58:10 -0700

    Add find class method to search for specific volunteers; test pass

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -27,4 +27,11 @@ class Volunteer
     volunteers
   end
 
+  def self.find(id)
+    volunteer = DB.exec("SELECT * FROM volunteers WHERE id = #{id};").first()
+    name = volunteer.fetch("name")
+    id = volunteer.fetch("id").to_i
+    project_id = volunteer.fetch("project_id").to_i
+    Volunteer.new({:name => name, :id => id, :project_id => project_id})
+  end
 end
