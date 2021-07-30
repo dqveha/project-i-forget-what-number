63000254aee16e95cbd63111ff565f55db62af19
Dave Lindqvist <Lindqvist.Dave@gmail.com>
2021-07-30 08:28:02 -0700

    Add project class with initializer; test pass

---
+++ lib/project.rb
@@ -0,0 +1,11 @@
+class Project
+  attr_reader :id, :title
+
+  def initialize(attributes)
+    @title = attributes.fetch(:title)
+    @id = attributes.fetch(:id)
+  end
+
+  
+
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,7 @@ require "rspec"
 require "pry"
 require "pg"
 
-DB = PG.connect({:dbname => 'volunteer_tracker_test'})
+DB = PG.connect({:dbname => 'volunteer_tracker_test', host: 'db', user: 'postgres', password: 'password'})
 
 RSpec.configure do |config|
   config.after(:each) do
