a3049a9b03721d37feb9ae0965b0d18bcd1288f9
Dave Lindqvist <Lindqvist.Dave@gmail.com>
2021-07-30 09:48:09 -0700

    Add save instance method to volunteer class and display all class method; #save test pass

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -11,4 +11,20 @@ class Volunteer
     (self.name() == volunteer_to_compare.name()) && (self.project_id() == volunteer_to_compare.project_id())
   end
 
+  def save()
+    result = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', #{project_id}) RETURNING id;")
+    @id = result.first().fetch("id").to_i
+  end
+
+  def self.all()
+    returned_volunteers = DB.exec("SELECT * FROM volunteers;")
+    volunteers = returned_volunteers.map() do |volunteer|
+      name = volunteer.fetch("name")
+      id = volunteer.fetch("id").to_i
+      project_id = volunteer.fetch("project_id")
+      Volunteer.new({:name => name, :id => id, :project_id => project_id})
+    end
+    volunteers
+  end
+
 end
